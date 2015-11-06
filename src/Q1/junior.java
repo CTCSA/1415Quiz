929ac13bd15f1f565bfccc1b2a8afdbae3d82205
ckc <mail@ckc.im>
2015-11-06 20:01:52 +0800

    1

---
+++ src/Q1/junior.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Q1;
+
+/**
+ *
+ * @author ckc
+ */
+public class junior extends Staff{
+     double payPerHour = 0.0;
+     double salary = 0;
+}
